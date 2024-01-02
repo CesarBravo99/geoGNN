13d31d2a74c003e7f0a537317704dada01b8d400
cesarbravo99 <cibravo2@uc.cl>
2024-01-02 03:47:43 +0000

    graphSAGE

--- src/graphSAGE.py
+++ src/graphSAGE.py
@@ -5,45 +5,28 @@ import torch.nn.init as init
 
 
 class NeighborAggregator(nn.Module):
-    def __init__(self, input_dim, output_dim, 
-                 use_bias=False, aggr_method="mean"):
-        """
-        Args:
-            input_dim:
-            output_dim:
-            use_bias: (default: {False})
-            aggr_method: (default: {mean})
-        """
+    def __init__(self, input_dim, output_dim, use_bias=False, aggr_method="mean"):
         super(NeighborAggregator, self).__init__()
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.use_bias = use_bias
         self.aggr_method = aggr_method
         self.weight = nn.Parameter(torch.Tensor(input_dim, output_dim))
-        if self.use_bias:
-            self.bias = nn.Parameter(torch.Tensor(self.output_dim))
+        if self.use_bias: self.bias = nn.Parameter(torch.Tensor(self.output_dim))
         self.reset_parameters()
     
     def reset_parameters(self):
         init.kaiming_uniform_(self.weight)
-        if self.use_bias:
-            init.zeros_(self.bias)
+        if self.use_bias: init.zeros_(self.bias)
 
     def forward(self, neighbor_feature):
-        if self.aggr_method == "mean":
-            aggr_neighbor = neighbor_feature.mean(dim=1)
-        elif self.aggr_method == "sum":
-            aggr_neighbor = neighbor_feature.sum(dim=1)
-        elif self.aggr_method == "max":
-            aggr_neighbor = neighbor_feature.max(dim=1)
-        else:
-            raise ValueError("Unknown aggr type, expected sum, max, or mean, but got {}"
+        if self.aggr_method == "mean": aggr_neighbor = neighbor_feature.mean(dim=1)
+        elif self.aggr_method == "sum": aggr_neighbor = neighbor_feature.sum(dim=1)
+        elif self.aggr_method == "max": aggr_neighbor = neighbor_feature.max(dim=1)
+        else: raise ValueError("Unknown aggr type, expected sum, max, or mean, but got {}"
                              .format(self.aggr_method))
-        
         neighbor_hidden = torch.matmul(aggr_neighbor, self.weight)
-        if self.use_bias:
-            neighbor_hidden += self.bias
-
+        if self.use_bias: neighbor_hidden += self.bias
         return neighbor_hidden
 
     def extra_repr(self):

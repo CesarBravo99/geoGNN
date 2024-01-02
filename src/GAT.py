9666ff7d128ac7359d1a763d1be39d7c476860a3
CesarBravo99 <cibravo2@uc.cl>
2024-01-02 06:15:40 -0300

    style(GAT): :art: Delte useless lines

--- src/GAT.py
+++ src/GAT.py
@@ -22,22 +22,18 @@ class GraphConvolution(nn.Module):
         self.output_dim = output_dim
         self.use_bias = use_bias
         self.weight = nn.Parameter(torch.Tensor(input_dim, output_dim))
-        if self.use_bias:
-            self.bias = nn.Parameter(torch.Tensor(output_dim))
-        else:
-            self.register_parameter('bias', None)
+        if self.use_bias: self.bias = nn.Parameter(torch.Tensor(output_dim))
+        else: self.register_parameter('bias', None)
         self.reset_parameters()
 
     def reset_parameters(self):
         init.kaiming_uniform_(self.weight)
-        if self.use_bias:
-            init.zeros_(self.bias)
+        if self.use_bias: init.zeros_(self.bias)
 
     def forward(self, adjacency, input_feature):
         support = torch.mm(input_feature, self.weight)
         output = torch.sparse.mm(adjacency, support)
-        if self.use_bias:
-            output += self.bias
+        if self.use_bias: output += self.bias
         return output
 
     def __repr__(self):

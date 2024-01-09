4a0561cbfb6a568f58896fb98082a1065cb70ea7
César Bravo <60265515+CesarBravo99@users.noreply.github.com>
2024-01-08 22:11:54 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 EXIT_CODE=0
-echo "Paperspace setup - Starting notebook setup"
-PIP_DISABLE_PIP_VERSION_CHECK=1 
-echo "Paperspace setup - Starting Jupyter kernel"
-jupyter lab --allow-root --ip=0.0.0.0 --no-browser --ServerApp.trust_xheaders=True \
-            --ServerApp.disable_check_xsrf=False --ServerApp.allow_remote_access=True \
-            --ServerApp.allow_origin='*' --ServerApp.allow_credentials=True
+
+echo "Paperspace setup - Starting Python setup"
+cp -a /storage/dist-packages/. z/usr/local/lib/python3.9/dist-packages/
+
 exit $EXIT_CODE

3d8dcd094b95efbe1234292dd4ea0243e6308458
César Bravo <60265515+CesarBravo99@users.noreply.github.com>
2024-01-08 23:05:10 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 EXIT_CODE=0
 
 echo "Paperspace setup - Starting Python setup"
-cp -a /storage/dist-packages/. z/usr/local/lib/python3.9/dist-packages/
+cp -a /storage/dist-packages/. /usr/local/lib/python3.9/dist-packages/
 
 exit $EXIT_CODE

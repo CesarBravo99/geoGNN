33c233ccead96ce869489f42f811241c2951a1ed
cesarbravo99 <cibravo2@uc.cl>
2024-01-15 05:26:06 +0000

    new

--- src/basicGNN.py
+++ src/basicGNN.py
@@ -32,25 +32,17 @@ class basicGNN(torch.nn.Module):
         self.mlp = mlp
         self.pooling = pooling
         
-        self.conv = torch.nn.ModuleList([])
+        self.conv_layers = torch.nn.ModuleList([GraphConv(in_channels, hidden_channels[0])])
         for i in range(len(hidden_channels)-1):
-            if i == 0:
-                self.conv.append(GCNConv(in_channels, hidden_channels[i]))
-            else:
-                self.conv.append(GCNConv(hidden_channels[i], hidden_channels[i+1]))
+            self.conv_layers.append(GraphConv(hidden_channels[i], hidden_channels[i+1]))
         if mlp:
             self.final_layer = Linear(hidden_channels[-1], out_channels)
         else:
-            self.final_layer = GCNConv(hidden_channels[-1], out_channels)
+            self.final_layer = GraphConv(hidden_channels[-1], out_channels)
             
             
-        # self.conv1 = GraphConv(in_channels, hidden_channels)
-        # self.conv2 = GraphConv(hidden_channels, hidden_channels)
-        # self.conv3 = GraphConv(hidden_channels, hidden_channels)
-        # self.lin = Linear(hidden_channels, out_channels)
-
     def forward(self, x, edge_index, batch=None):
-        for conv_layer in self.conv:
+        for conv_layer in self.conv_layers:
             x = conv_layer(x, edge_index)
             x = x.relu()
             
@@ -63,19 +55,5 @@ class basicGNN(torch.nn.Module):
             x = self.final_layer(x)
         else:
             x = self.final_layer(x, edge_index)
-        
-        # 1. Obtain node embeddings 
-        # x = self.conv1(x, edge_index)
-        # x = x.relu()
-        # x = self.conv2(x, edge_index)
-        # x = x.relu()
-        # x = self.conv3(x, edge_index)
-
-        # 2. Readout layer
-        # x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
-
-        # 3. Apply a final classifier
-        # x = F.dropout(x, p=0.5, training=self.training)
-        # x = self.lin(x)
         
         return x
